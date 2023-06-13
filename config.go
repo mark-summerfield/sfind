391716e83ace1adc0e233e400b5d34438c5140cf
mark-summerfield <mark@qtrac.eu>
2023-06-13 08:56:51 +0100

    more

--- config.go
+++ config.go
@@ -14,11 +14,12 @@ import (
 )
 
 func getConfig() *config {
+	const comma = ","
 	parser := makeParser()
-	fromOpt := parser.Str("from", fromDesc, "")
 	suffixesOpt := parser.Str("suffixes", suffixesDesc, "")
-	globsOpt := parser.Str("glob", globsDesc, "")
 	containsOpt := parser.Str("contains", containsDesc, "")
+	globsOpt := parser.Str("glob", globsDesc, "")
+	fromOpt := parser.Str("from", fromDesc, "")
 	excludeOpt := parser.Str("exclude", excludeDesc, "")
 	excludeOpt.SetShortName('x')
 	debugOpt := parser.Flag("debug", "print config and quit")
@@ -32,18 +33,20 @@ func getConfig() *config {
 	}
 	globs := make([]string, 0)
 	if globsOpt.Given() {
-		globs = strings.Split(globsOpt.Value(), ",")
+		globs = strings.Split(globsOpt.Value(), comma)
 	}
 	if containsOpt.Given() {
-		globs = append(globs, fmt.Sprintf("*%s*", containsOpt.Value()))
+		for _, contains := range strings.Split(containsOpt.Value(), comma) {
+			globs = append(globs, fmt.Sprintf("*%s*", contains))
+		}
 	}
 	suffixes := make([]string, 0)
 	if suffixesOpt.Given() {
-		suffixes = strings.Split(suffixesOpt.Value(), ",")
+		suffixes = strings.Split(suffixesOpt.Value(), comma)
 	}
 	updateGlobs(parser, globs, suffixes, config)
 	if excludeOpt.Given() {
-		config.excludes = strings.Split(excludeOpt.Value(), ",")
+		config.excludes = strings.Split(excludeOpt.Value(), comma)
 	}
 	if len(parser.Positionals) > 0 {
 		config.paths = parser.Positionals
@@ -138,8 +141,9 @@ const (
 	suffixesDesc = "The comma-separated file suffixes to match (e.g., " +
 		"py,pyw) [default: any file]."
 	globsDesc = "The comma-separated file globs to match (e.g., " +
-		"'*.py,*.pyw') [default: any file]."
-	containsDesc = "The file names that contain CONTAINS (shorthand " +
-		"for -g '*CONTAINS*' --)."
-	excludeDesc = "Paths to exclude [default: none]."
+		"'*.tcl,*.tm') [default: any file]."
+	containsDesc = "The comma-separated file names that contain CONTAINS " +
+		"(e.g., -c 'readme,README' is the same as " +
+		"-g '*readme*,*README*')."
+	excludeDesc = "The comma-separated paths to exclude [default: none]."
 )
